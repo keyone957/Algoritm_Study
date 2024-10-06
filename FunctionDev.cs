a6fc463fbc6e242224ef773552b2183fc40b660b
Hone Won Gi <keyone957@naver.com>
2024-10-06 21:10:53 +0900

    프로그래머스 : 기능개발

---
+++ FunctionDev.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class FunctionDev
+{
+    public int[] solution(int[] progresses, int[] speeds)
+    {
+        List<int> answer = new List<int>();
+        Queue<int> Q_progresses = new Queue<int>(progresses);
+        Queue<int> Q_speeds = new Queue<int>(speeds);
+        int complete = 0;
+
+        while (Q_progresses.Count > 0)
+        {
+            for (int i = 0; i < Q_progresses.Count; i++)
+            {
+                int temp = Q_progresses.Dequeue();
+                temp += Q_speeds.ToArray()[i];
+
+                Q_progresses.Enqueue(temp);
+            }
+
+
+            while (Q_progresses.Peek() >= 100)
+            {
+                Q_progresses.Dequeue();
+                Q_speeds.Dequeue();
+                complete++;
+                if (Q_progresses.Count == 0) break;
+            }
+            if (complete > 0)
+            {
+                answer.Add(complete);
+                complete = 0;
+            }
+        }
+
+        return answer.ToArray();
+    }
+}
